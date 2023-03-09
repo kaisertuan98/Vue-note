4e2a4d59c449086e8503991328fb6c8aa22bd223
kaisertuan <thekaiser.tuan@gmail.com>
2023-03-09 22:49:53 +0700

    create form

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,32 @@
 <template>
-  <learning-resource
-    v-for="res in storedResources"
-    :key="res.id"
-    :title="res.title"
-    :description="res.description"
-    :link="res.link"
-  ></learning-resource>
+  <the-header title="RememberMe"></the-header>
+  <the-resources></the-resources>
 </template>
 
 <script>
-import LearningResource from './components/learning-resource/LearningResource.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resource/TheResource.vue';
 
 export default {
   components: {
-    LearningResource,
-  },
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation',
-          link: 'https://vuejs.org',
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to google...',
-          link: 'https://google.com',
-        },
-      ],
-    };
+    TheHeader,
+    TheResources,
   },
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
